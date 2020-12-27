4e0daa77b10b52ed84b338832361a65341cb7a45
Martin Martinez <investingmtz@gmail.com>
2020-12-27 12:35:05 -0600

    Agregaré recursividad..

--- collatz.py
+++ collatz.py
@@ -1,9 +1,16 @@
-def collatz(number):
-    if number % 2 == 0:
-        print(number)
-    if number & 2 == 1:
-        print(3 * number + 1)
+def collatz(numero):
+    if numero % 2 == 0:
+        return numero / 2
+    if numero & 2 == 1:
+        return numero * 3 + 1
 
+def main():
+    numeroUsuario = int(input('Ingresa un numero entero: '))
+    terminar = 0
+    
+    while terminar != 1:
+        collatz(collatz(numeroUsuario))
 
-if __name__ == "__main__":
-    collatz()
+        
+    
+main()
