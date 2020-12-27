c51d56b08042c4dc3f5b04aa6d15a6f4d9d436d3
Martin Martinez <investingmtz@gmail.com>
2020-12-27 13:05:58 -0600

    Agregando punto de ejecucion

--- collatz.py
+++ collatz.py
@@ -5,10 +5,13 @@ def collatz(num):
     else:
         num = num * 3 + 1
         return num 
+def main():
+    numero_usuario = input('Ingresa un entero: ')
+    numero_usuario = int(numero_usuario)
 
-numero_usuario = input('Ingresa un entero: ')
-numero_usuario = int(numero_usuario)
+    while numero_usuario > 1:
+        print(collatz(numero_usuario))
+        numero_usuario = collatz(numero_usuario)
 
-while numero_usuario > 1:
-    print(collatz(numero_usuario))
-    numero_usuario = collatz(numero_usuario)
+if __name__ == "__main__":
+    main()
